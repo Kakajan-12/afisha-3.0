9b2889536b2d022d0370c2f6146aa79002086cbc
Kakajan <kakajantayjikov@gmail.com>
2024-11-06 08:42:12 +0500

    success redirect2

--- js/redirect.js
+++ js/redirect.js
@@ -2,7 +2,7 @@ const getPath = window.location.href;
 function autoRedirect() {
   setTimeout(function () {
     window.location.href = getPath + "second.html";
-  }, 7000);
+  }, 6000);
 }
 
 autoRedirect();
